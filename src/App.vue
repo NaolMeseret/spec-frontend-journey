5f3664657f5555c259479231eab680e49b7ade6d
Naol <naolmes15062015@gmail.com>
2026-02-04 17:50:21 +0300

    day-2 task

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <script setup>
 import { ref } from "vue"
+
 const name = ref("")
 </script>
 
 <template>
-  <input v-model="name" placeholder="Type your name" />
-  <p>Hello {{ name }}</p>
+  <div>
+    <input type="text" v-model="name" placeholder="Enter your name" />
+
+    <h2 v-if="name">Hello {{ name }}</h2>
+  </div>
 </template>
